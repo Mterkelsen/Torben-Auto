4430567aee781d81836f1e879ba3fc341650033a
unknown <Jens@Christian-Pc.thorvalddata.com>
2016-02-03 12:38:29 +0100

    frontpage boxes, hover effects

--- torben/forside.php
+++ torben/forside.php
@@ -9,8 +9,8 @@ require_once('../includes/functions.php');
 
 <div class="container">
     <div class="col-md-12 no-padding bg-white">
-        <div class="col-md-6 bg-mek text-center"><a href="#" class="mekaniker_a">Sørg for at din bil bliver synet</a></div>
-        <div class="col-md-6 bg-vet text-center"><a href="#" class="veteran_a">Veteran bil renovation</a></div>
+        <div class="col-md-6 bg-mek text-center mekaniker_a"><a href="#" class="hvr-bounce-to-bottom"><br><br>Sørg for at din bil bliver synet<br><br><br><br><button class="btn btn-default mek_read_more" href="#"><p>Læs mere her!</p></button><br><br><br><br></a></div>
+        <div class="col-md-6 bg-vet text-center veteran_a"><a href="#" class="hvr-bounce-to-bottom"><br><br>Veteran bil renovation<br><br><br><br><br><button class="btn btn-default vet_read_more" href="#"><p>Læs mere her!</p></button><br><br><br></a></div>
     </div>
 </div>
 
