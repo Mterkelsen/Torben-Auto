4847331c40a82b8ffceac77770455732353de56f
unknown <Jens@Christian-Pc.thorvalddata.com>
2016-02-03 14:19:17 +0100

    no message

--- torben/forside.php
+++ torben/forside.php
@@ -12,7 +12,7 @@ require_once('../includes/functions.php');
         <div class="col-md-6 bg-mek text-center mekaniker_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Sørg for at din bil bliver synet<br><br><br><br><button class="btn btn-default mek_read_more" href="#"><p>Kontakt Torben her!</p></button><br><br><br><br><br></a></div>
         <div class="col-md-6 bg-vet text-center veteran_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Veteran bil renovation<br><br><br><br><button class="btn btn-default vet_read_more" href="#"><p>Læs mere her!</p></button><br><br><br><br><br></a></div>
     </div>
-    <div class="col-md-12 no-padding bg-grey" style="margin-top:-2px;">
+    <div class="col-md-12 no-padding bg-grey bg-alum" style="margin-top:-2px;">
         <h2 class="text-center">Nyheder</h2>
 
     </div>
