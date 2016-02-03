ba6ae1da162d0ccdb1ef0c36664e42c2d6041b95
unknown <Jens@Christian-Pc.thorvalddata.com>
2016-02-03 11:27:31 +0100

    2 forside kasser tilføjer column 6, tilføjet ahref til mekaniker delen og veteran delen

--- torben/forside.php
+++ torben/forside.php
@@ -7,13 +7,11 @@ require_once('../includes/functions.php');
 ?>
 <?php require_once('../torben/templates/frontend_header.php') ?>
 
-<div class="container no-padding">
-    <a href="/torben/mekaniker.php" class="floating-text" role="button">
-        
-    </a>
-    <a href="/torben/veteran.php" class="floating-text" role="button">
-
-    </a>
+<div class="container">
+    <div class="col-md-12 no-padding bg-white">
+        <div class="col-md-6 bg-mek text-center"><a href="#" class="mekaniker_a">Sørg for at din bil bliver synet</a></div>
+        <div class="col-md-6 bg-vet text-center"><a href="#" class="veteran_a">Veteran bils rennovation</a></div>
+    </div>
 </div>
 
 </body>
