d07ad1e5c70b04f71a3d715cead931e97c0d7a4f
Matthias Worm Terkelsen <matt0871@easv365.dk>
2016-02-04 15:25:45 +0100

    Edited frontpage news section, added php code to include the 2 latest news and link to the news site via a button

--- torben/forside.php
+++ torben/forside.php
@@ -9,14 +9,24 @@ require_once('../includes/functions.php');
 
 <div class="container">
     <div class="col-md-12 no-padding bg-white">
-        <div class="col-md-6 bg-mek text-center mekaniker_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Sørg for at din bil bliver synet<br><br><br><br><button class="btn btn-default mek_read_more" href="#"><p>Kontakt Torben her!</p></button><br><br><br><br><br></a></div>
-        <div class="col-md-6 bg-vet text-center veteran_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Veteran bil renovation<br><br><br><br><button class="btn btn-default vet_read_more" href="#"><p>Læs mere her!</p></button><br><br><br><br><br></a></div>
+        <div class="col-md-6 bg-mek text-center mekaniker_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Sørg for at din bil bliver synet<br><br><br><br><button class="btn btn-default mek_read_more hvr-float-shadow" href="#"><p>Kontakt Torben her!</p></button><br><br><br><br><br></a></div>
+        <div class="col-md-6 bg-vet text-center veteran_a no-padding"><a href="#" class="hvr-bounce-to-bottom"><br><br>Veteran bil renovation<br><br><br><br><button class="btn btn-default vet_read_more hvr-float-shadow" href="#"><p>Læs mere her!</p></button><br><br><br><br><br></a></div>
     </div>
-    <div class="col-md-12 no-padding bg-grey bg-dark" style="margin-top:-2px;">
-        <h5 class="text-center mg-top">Nyheder</h5>
+    <div class="col-md-12 no-padding bg-opacity-darker bg-cars">
+        <h5 class="text-center mg-top">NYHEDER</h5>
+        <hr class="hr-news"> <i class="fa fa-newspaper-o fa-3x hr-newsicon bg-opacity-darker bg-cars cl-yellow"></i> </hr>
+        <?php
+
+        $sql = mysqli_query($connection, "SELECT news_header, news_content, news_image FROM news ORDER BY news_date DESC LIMIT 2");
 
 
+        while($NewsArray = mysqli_fetch_array($sql)){
 
+            echo "<div class='col-md-6'>"."<h2 class='bold'>".$NewsArray['news_header']."</h2>".
+                "\n"."<p>".$stringcut = substr($NewsArray['news_content'],0,350)."..</p>"."<br>"."<button class=\"btn btn-default vet_read_more hvr-float-shadow\" href=\"#\"><p>Læs mere her!</p></button>"."</div>";
+
+        }
+        ?>
     </div>
 </div>
 
--- torben/templates/frontend_header.php
+++ torben/templates/frontend_header.php
@@ -7,6 +7,7 @@
     <link href="/torben/css/bootstrap.css" rel="stylesheet" />
     <link href="/torben/css/style.css" rel="stylesheet" />
     <link href="/torben/css/hover.css" rel="stylesheet" media="all">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="/torben/js/bootstrap.js"></script>
     <script src="http://maps.googleapis.com/maps/api/js"></script>
@@ -22,13 +23,13 @@
     <a href="/torben/forside.php" ><img class="logo center-block" src="/torben/images/logo/logo.png" /></a>
 </div>
 <div class="container">
-<div id="custom-bootstrap-menu" class="navbar navbar-default bg-grey bg-dark" role="navigation">
+<div id="custom-bootstrap-menu" class="navbar navbar-default bg-opacity" role="navigation">
 
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-menubuilder"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
             </button>
         </div>
-        <div class="collapse navbar-collapse navbar-menubuilder bg-dark">
+        <div class="collapse navbar-collapse navbar-menubuilder bg-transparent">
             <ul class="nav navbar-nav navbar-right ">
                 <li ><a href="forside.php" class="hvr-sweep-to-top">Forside</a></li>
                 <li ><a href="nyheder.php" class="hvr-sweep-to-top">Nyheder</a></li>
