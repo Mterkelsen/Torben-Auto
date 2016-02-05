0feb3e51a5b8e5b2df73c33af3976090ea1a2ab5
Matthias Worm Terkelsen <matt0871@easv365.dk>
2016-02-05 15:14:05 +0100

    Ændringer til forsiden, news og 3 info bokse

--- torben/forside.php
+++ torben/forside.php
@@ -28,6 +28,98 @@ require_once('../includes/functions.php');
         }
         ?>
     </div>
+
+</div>
+<div class="container-fluid no-padding bg-horizontal mg-top">
+<div class="container">
+    <div class="col-md-12 no-padding mg-top mg-bottom">
+        <div class="col-md-4 col-sm-4 newsbox">
+            <div class="minibox_header">
+                <h6>News</h6>
+            </div>
+            <div class="minibox_body bg-opacity">
+                <?php
+
+                $sql = mysqli_query($connection, "SELECT news_header, news_content, news_image FROM news ORDER BY news_date DESC LIMIT 2");
+
+
+                while($NewsArray = mysqli_fetch_array($sql)){
+
+
+                    echo "<h2 class='bold'>".$NewsArray['news_header']."</h2>"."<hr class='news_hr'>"."\n"."<p>".$stringcut = substr($NewsArray['news_content'],0,150)."..</p>"."<br>";}
+
+
+
+
+
+                ?>
+            </div>
+            <a href="news.php"><div class="minibox_footer">
+                    <button class="btn-success hvr-shutter-out-vertical">Read more</button>
+                </div>
+        </div></a>
+
+
+        <div class="col-md-4 col-sm-4 weeklybox">
+            <div class="minibox_header">
+                <h6>Weekly Special</h6>
+            </div>
+            <div class="minibox_body bg-opacity">
+                <?php
+
+                $MenuSql = mysqli_query($connection, "SELECT menu_name, menu_description, menu_price FROM menus WHERE daily='1'");
+
+
+                while($MenuArray = mysqli_fetch_array($MenuSql)){
+
+
+                    echo "<h2 class='bold'>".$MenuArray['menu_name']."</h2>"."<hr class='news_hr'>"."\n"."<p>".$stringcut = substr($MenuArray['menu_description'],0,300)."</p>"."<br>\n"."<h2>".$MenuArray['menu_price']."</h2>";
+
+                }
+
+
+
+
+
+                ?>
+            </div>
+            <a href="menu.php"><div class="minibox_footer">
+                    <button class="btn-success hvr-shutter-out-vertical">Read more</button>
+
+                </div></a>
+        </div>
+
+
+        <div class="col-md-4 col-sm-4 gamebox">
+            <div class="minibox_header">
+                <h6>Upcoming Events</h6>
+            </div>
+            <div class="minibox_body bg-opacity">
+                <?php
+
+                $EventSql = mysqli_query($connection, "SELECT news_header FROM news WHERE news_category='events' LIMIT 4");
+
+
+                while($EventArray = mysqli_fetch_array($EventSql)){
+
+                    echo "<h2 class='bold'>".$EventArray['news_header']."</h2>"."\n"."<hr  class='news_hr'>";
+
+                }
+
+
+
+
+
+                ?>
+
+            </div>
+            <a href="news.php"><div class="minibox_footer">
+                    <button class="btn-success hvr-shutter-out-vertical">Read more</button>
+
+                </div></a>
+        </div>
+    </div>
+</div>
 </div>
 
 </body>
--- torben/includes/functions.php
+++ torben/includes/functions.php
@@ -4,3 +4,21 @@
         header("Location: {$location}");
         exit;
     }
+
+    function sanitize($conn, $data)
+
+    {
+
+        $data = trim($data);
+
+        $data = strip_tags($data);
+
+        $data = stripslashes($data);
+
+        $data = htmlspecialchars($data);
+
+        mysqli_real_escape_string($conn, $data);
+
+        return $data;
+
+    }
