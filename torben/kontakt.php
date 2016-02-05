ce01b42f9c04ab6f7f3597e9fe910cbf75e3d24f
Anders Bergmann <ande912g@easv365.dk>
2016-02-05 15:36:39 +0100

    Kontakt-style

--- torben/kontakt.php
+++ torben/kontakt.php
@@ -8,23 +8,23 @@ require_once('includes/openinghours.php')
 ?>
 <?php require_once('../torben/templates/frontend_header.php') ?>
 
+
 <div class="container mg-top">
         <div class="col-md-12 bg-opacity border-yellow">
-            <div name="col-center" class="col-md-8">
+            <div class="col-md-12 col-center mg-top">
                 <section id="contact">
 
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-8">
+
+                            <div class="col-lg-12">
                                 <div class="row">
-                                    <div class="col-lg-12 text-center">
-                                        <h2>Kontakt</h2>
+                                    <div class="col-lg-12 text-center"">
+                                        <h3 class="text-center">Kontakt</h3>
                                     </div>
                                 </div>
                                 <!-- To configure the contact form email address, go to mail/contact_me.php and update the email address in the PHP file on line 19. -->
                                 <!-- The form should work on most web servers, but if the form is not working you may need to configure your web server differently. -->
                                 <form name="sentMessage" id="contactForm" novalidate="">
-                                    <div class="row control-group">
+                                    <div class="row control-group mg-top">
                                         <div class="form-group col-xs-12 floating-label-form-group controls">
 
                                             <input aria-invalid="false" class="form-control" placeholder="Navn" id="navn" required="" data-validation-required-message="Indtast dit navn." type="text">
@@ -61,14 +61,13 @@ require_once('includes/openinghours.php')
                                     </div>
                                 </form>
                             </div>
-                        </div>
-                    </div>
 
-                </section>
-            </div>
-            <div class="col-md-6">
+
+
+        </div>
+            <div class="col-md-12">
                 <h3 class="text-center">Åbningstider</h3>
-                    <div class="col-md-6">
+                    <div class="col-md-12">
 
                         <?php
                         $HoursQuery =  "SELECT * FROM openinghours";
@@ -76,24 +75,31 @@ require_once('includes/openinghours.php')
                         $LoadHours = mysqli_query($connection, $HoursQuery);
 
                         while($HoursView = mysqli_fetch_array($LoadHours)){
-                            echo "<h2 class='text-left'>Mandag - ".$HoursView['Mandag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Tirsdag - ".$HoursView['Tirsdag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Onsdag - ".$HoursView['Onsdag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Torsdag - ".$HoursView['Torsdag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Fredag - ".$HoursView['Fredag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Lørdag - ".$HoursView['Lørdag']."</h2>"."</br>".
-                                "<h2 class='text-left'>Søndag - ".$HoursView['Søndag']."</h2>"."</br>";
+                            echo "<h2 class='text-center'>Mandag - ".$HoursView['Mandag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Tirsdag - ".$HoursView['Tirsdag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Onsdag - ".$HoursView['Onsdag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Torsdag - ".$HoursView['Torsdag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Fredag - ".$HoursView['Fredag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Lørdag - ".$HoursView['Lørdag']."</h2>"."</br>".
+                                "<h2 class='text-center'>Søndag - ".$HoursView['Søndag']."</h2>"."</br>";
                         }
                         ?>
-                        <div class="col-md-6 col-sm-6 col-xs-6"><h2 class="text-left">Telefon:</br> E-mail: </br> Adresse:</h2></div>
-                        <div class="col-md-6 col-sm-6 col-xs-6"><h2 class="text-left">75 39 85 06 / 21 64 51 06  </br> Torren2708@hotmail.com </br> Bobøl-Foldingbrovej 11, Bobøl, 6683 Føvling</h2> </div>
+
+                        <div class="col-md-12 col-sm-12 col-xs-12"><h2 class="text-center">Telefon:</br> E-mail: </br> Adresse:</h2>
+                        </div>
+                        <div class="col-md-12 col-sm-12 col-xs-12"><h2 class="text-center">75 39 85 06 / 21 64 51 06  </br> Torren2708@hotmail.com </br> Bobøl-Foldingbrovej 11, Bobøl, 6683 Føvling</h2>
+                        </div>
                     </div>
              </div>
 
-            <div class="col-md-12 center-block mg-bot"><iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d3917.9877066892777!2d8.916957302634483!3d55.43949695331402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d55.4399173!2d8.919628399999999!5e0!3m2!1sda!2sdk!4v1454501055905" width="100%" height="500px" frameborder="0" style="border:0; z-index:-1; pointer-event:none;"></iframe></div>
+            <div class="col-md-12 center-block mg-bot"><iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d3917.9877066892777!2d8.916957302634483!3d55.43949695331402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d55.4399173!2d8.919628399999999!5e0!3m2!1sda!2sdk!4v1454501055905" width="100%" height="500px" frameborder="0" style="border:0; z-index:-1;"></iframe></div>
         </div>
-    </div>
 
 
+<style>
+    .col-center{
+
+    }
+</style>
 </body>
 </html>
